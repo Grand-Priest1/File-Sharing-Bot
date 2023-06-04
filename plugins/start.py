fe5263c59efbbc0cdb411bbcc7e8ec95cb918824
Grand-Priest1 <125681114+Grand-Priest1@users.noreply.github.com>
2023-06-04 13:48:46 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -88,8 +88,8 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
+                    InlineKeyboardButton("🔥 About Me 🔥", callback_data = "about"),
+                    InlineKeyboardButton("☠️ Close", callback_data = "close")
                 ]
             ]
         )
@@ -123,7 +123,7 @@ async def not_joined(client: Client, message: Message):
     buttons = [
         [
             InlineKeyboardButton(
-                "Join Channel",
+                "🎬 Join 🎬",
                 url = client.invitelink)
         ]
     ]
